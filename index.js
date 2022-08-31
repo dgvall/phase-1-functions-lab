c5fa3b450f425dd131ed64d2e3712910cbd2b177
Diego Valles <dgvall134@gmail.com>
2022-08-31 12:07:42 -0600

    commit tree b6d5ef72b22ce52642e03757428f25f8942e3903

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    let result = someValue - 42;
+    return Math.abs(result)
+}
+
+function distanceFromHqInFeet(someValue) {
+    let feet = distanceFromHqInBlocks(someValue) * 264
+    return feet
+}
+
+function distanceTravelledInFeet(start, destination) {
+    let blocksTravelled = destination - start;
+    let feetTravelled = blocksTravelled * 264;
+    return Math.abs(feetTravelled)
+}
+
+function calculatesFarePrice(start, destination) {
+    let distance = distanceTravelledInFeet(start, destination);
+    if (distance <= 400) {
+        return 0
+    } else if (distance <= 2000) {
+        return (distance - 400)*.02
+    } else if (distance <= 2500) {
+        return 25
+    } else if (distance > 2500) {
+        return `cannot travel that far`
+    }
+}
